db8dc8276611e5af31e26e6f7541f50ef981f826
Daniel Dawson <dpdawson@dpdawson.org>
2016-02-17 22:46:07 +0100

    Don't inject stripe script tag in tests.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 
 module.exports = {
   name: 'stripe-service',
-  contentFor: function(type) {
+  contentFor: function(type, config) {
     // we use body since Stripe must be available before
-    if (type === 'body') {
+    if (type === 'body' && config.environment !== 'test') {
       return '<script type="text/javascript" src="https://js.stripe.com/v2/"></script>';
     }
   }
